d18b4a0d768ee60206c638e9c24c8e8b7fd4b510
lifly <1289142675@qq.com>
2022-04-16 10:21:05 +0800

    增加日志和token认证

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"github.com/beego/beego/v2/adapter/logs"
 	beego "github.com/beego/beego/v2/server/web"
 	_ "wxEmoji/routers"
 )
 
 func main() {
+	err := logs.SetLogger(logs.AdapterFile, `{"filename":"run.log"}`)
+	if err != nil {
+		logs.Error("日志配置失败")
+	}
 	beego.Run()
 }
