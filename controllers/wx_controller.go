486c3a2cbf905ca5fd984f1ff7de38ce8b2c4d19
lifly <1289142675@qq.com>
2022-04-18 21:30:45 +0800

    增加日志和token认证

--- controllers/wx_controller.go
+++ controllers/wx_controller.go
@@ -4,9 +4,7 @@ import (
 	"crypto/sha1"
 	"github.com/beego/beego/v2/adapter/logs"
 	beego "github.com/beego/beego/v2/server/web"
-	"reflect"
 	"sort"
-	"wxEmoji/models"
 )
 
 type WxController struct {
@@ -14,17 +12,10 @@ type WxController struct {
 }
 
 func (c *WxController) Get() {
-	params := models.WxParam{}
-	if err := c.ParseForm(&params); err != nil {
-		c.Ctx.WriteString("发生错误")
-		logs.Error("token验证失败")
-	}
-	data := reflect.TypeOf(params)
-	filed := data.Field(0)
 	signature := c.GetString("signature", "未成功")
-	timestamp := filed.Tag.Get("timestamp")
-	nonce := filed.Tag.Get("nonce")
-	echostr := filed.Tag.Get("echostr")
+	timestamp := c.GetString("timestamp")
+	nonce := c.GetString("nonce")
+	echostr := c.GetString("echostr")
 	token := "yes"
 
 	list0 := []string{token, timestamp, nonce}
@@ -34,7 +25,6 @@ func (c *WxController) Get() {
 		sha.Write([]byte(list0[i]))
 	}
 	bs := sha.Sum(nil)
-	logs.Info("singnature=" + signature)
 	if string(bs) == signature {
 		c.Ctx.WriteString(echostr)
 	} else {
